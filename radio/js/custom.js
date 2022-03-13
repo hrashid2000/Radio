4f40d30a7f41aeaf436e9b18fdaa50712bb6cb2c
hrashid2000 <56900988+hrashid2000@users.noreply.github.com>
2022-03-13 16:16:16 -0400

    Add files via upload

--- radio/js/custom.js
+++ radio/js/custom.js
@@ -36,49 +36,44 @@ function addstream(name,url){
     document.body.appendChild(newDiv);
 }
 /*
-const Http = new XMLHttpRequest();
-//const url='http://ssh.noglider.com:8081/user1.csv';
-const url='https://jsonplaceholder.typicode.com/posts';
-Http.open("GET", url);
-Http.send();
+const CSVToArray = (data, delimiter = ",", omitFirstRow = false) =>
+    data
+        .slice(omitFirstRow ? data.indexOf("\n") + 1 : 0)
+        .split("\n")
+        .map(v => v.split(delimiter));
+*/        
+function csvToArray(str, delimiter = ",") {
+  
+  //const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+  //console.log(headers);
+  const headers = ['Name','Url'];
 
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
+  const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+  const arr = rows.map(function (row) {
+    const values = row.split(delimiter);
+    const el = headers.reduce(function (object, header, index) {
+      object[header] = values[index];
+      return object;
+    }, {});
+    return el;
+  });
+
+  return arr;
 }
-*/
 async function getData(){
     const response = await fetch('http://ssh.noglider.com:8081/user1.csv');
-    //const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.text();
+    var data = await response.text();
     console.log(data);
-
+    var final = csvToArray(data);
+    for (let i = 0; i< (final.length)-1; i++) {
+      addstream(final[i].Name, final[i].Url);
+    }
+  return;
 }
 getData();
-/*function addstream(img,name,url){ 
-var newDiv = document.createElement("div")
 
-const image = new Image();
-image.src = "media/icon.jpg";
-image.width = 100;
-image.height = 100;
-newDiv.appendChild(image);
 
-newDiv.innerHTML += name;
 
-const audio = new Audio();
-audio.id = "player";
-audio.src = url;
-newDiv.appendChild(audio);
 
-btn = document.createElement("button");
-btn.innerHTML += "Play &#9205;";
-btn.setAttribute('class', 'play');
-btn.type = "button";
-btn.setAttribute('onclick', 'playAudio(this)');
-newDiv.appendChild(btn);
 
-document.body.appendChild(newDiv);
-}*/
-
-//loadstream
-//for length(8) of csv do addstream
