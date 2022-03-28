851c497ff9b8c94fe4d655f13ffae0060766711e
hrashid2000 <56900988+hrashid2000@users.noreply.github.com>
2022-03-28 19:56:49 -0400

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,80 +1,91 @@
 function playAudio(btn) {
-    var stream = btn.parentElement.querySelector('audio');
-    var playorpause = btn.getAttribute('class');
+  var stream = btn.parentElement.querySelector('audio');
+  var playorpause = btn.getAttribute('class');
 
-    if (playorpause === 'play') {
-        stream.load();
-		stream.play();
-		btn.innerHTML = "Pause &#9208;";
-        btn.setAttribute('class', 'pause');
-    } else {
-        stream.pause();
-		btn.innerHTML = "Play &#9205;";
-        btn.setAttribute('class', 'play');
-    }
+  if (playorpause === 'play') {
+      stream.load();
+  stream.play();
+  btn.innerHTML = "Pause &#9208;";
+      btn.setAttribute('class', 'pause');
+  } else {
+      stream.pause();
+  btn.innerHTML = "Play &#9205;";
+      btn.setAttribute('class', 'play');
+  }
 
-    stream.onended = function () {
-        btn.setAttribute('class', 'play');
-    }
+  stream.onended = function () {
+      btn.setAttribute('class', 'play');
+  }
 }
-function addstream(name,url){ 
-    var newDiv = document.createElement("div")
-    
-    newDiv.innerHTML += name;
-    
-    const audio = new Audio();
-    audio.id = "player";
-    audio.src = url;
-    newDiv.appendChild(audio);
-    
-    btn = document.createElement("button");
-    btn.innerHTML += "Play &#9205;";
-    btn.setAttribute('class', 'play');
-    btn.setAttribute('onclick', 'playAudio(this)');
-    newDiv.appendChild(btn);
-    
-    document.body.appendChild(newDiv);
+function addstream(name,url,num){ 
+  var newDiv = document.createElement("div")
+  newDiv.id = "radio" + String(num);
+
+  newDiv.innerHTML += name;
+  
+  const audio = new Audio();
+  audio.id = "player" + String(num);
+  audio.src = url;
+  newDiv.appendChild(audio);
+  
+  btn = document.createElement("button");
+  btn.innerHTML += "Play &#9205;";
+  btn.setAttribute('class', 'play');
+  btn.setAttribute('onclick', 'playAudio(this)');
+  newDiv.appendChild(btn);
+  
+  document.body.appendChild(newDiv);
 }
 /*
 const CSVToArray = (data, delimiter = ",", omitFirstRow = false) =>
-    data
-        .slice(omitFirstRow ? data.indexOf("\n") + 1 : 0)
-        .split("\n")
-        .map(v => v.split(delimiter));
+  data
+      .slice(omitFirstRow ? data.indexOf("\n") + 1 : 0)
+      .split("\n")
+      .map(v => v.split(delimiter));
 */        
 function csvToArray(str, delimiter = ",") {
-  
-  //const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-  //console.log(headers);
-  const headers = ['Name','Url'];
 
-  const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+//const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+//console.log(headers);
+const headers = ['Name','Url'];
 
-  const arr = rows.map(function (row) {
-    const values = row.split(delimiter);
-    const el = headers.reduce(function (object, header, index) {
-      object[header] = values[index];
-      return object;
-    }, {});
-    return el;
-  });
+const rows = str.slice(str.indexOf("\n") + 1).split("\n");
 
-  return arr;
+const arr = rows.map(function (row) {
+  const values = row.split(delimiter);
+  const el = headers.reduce(function (object, header, index) {
+    object[header] = values[index];
+    return object;
+  }, {});
+  return el;
+});
+
+return arr;
 }
+
 async function getData(){
-    const response = await fetch('http://ssh.noglider.com:8081/user1.csv');
-    var data = await response.text();
-    console.log(data);
-    var final = csvToArray(data);
-    for (let i = 0; i< (final.length)-1; i++) {
-      addstream(final[i].Name, final[i].Url);
-    }
-  return;
+  const response = await fetch('http://ssh.noglider.com:8081/user1.csv');
+  var data = await response.text();
+  //console.log(typeof data);
+  //console.log(data);
+  var final = csvToArray(data);
+  //console.log(final);
+  //console.log(typeof final);
+  //console.log(final);
+  for (let i = 0; i< (final.length)-1; i++) {
+    addstream(final[i].Name, final[i].Url,i);
+  }
+  return final;
 }
-getData();
 
-
-
-
+//var data = [];
+var data = getData();
+const printAddress = async () => {
+  const a = await data;
+  console.log(a);
+};
+//printAddress();
+a = document.getElementById("player0");
+console.log(a);
 
 
